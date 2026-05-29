6f37abfedcd85ec1fa3b7c9159caaaf0efc6c564
Ahmad Mujtaba <mujtaba77007@gmail.com>
2026-05-29 17:25:57 +0500

    Update project-1.py

--- project-1.py
+++ project-1.py
@@ -22,32 +22,4 @@ else:
 
 
 
-"""
-🏠 Project 1 — Smart Home AC Controller Simulation
 
-Built a real-world IoT automation logic in Python 
-using conditional statements!
-
-🌡️ How it works:
-- Temp ≥ 45°C → Critical Heat! Max Power mode
-- Temp ≥ 30°C → Checks room occupancy:
-   - 5+ people → Turbo Mode activated
-   - Less people → Normal Mode (22°C)
-- Temp < 20°C → AC turns OFF (energy saving)
-- Otherwise → Comfortable, Eco Mode runs
-
-🔹 Concepts Practiced:
-✅ Nested if/elif/else conditions
-✅ Multiple user inputs
-✅ Real-world decision making logic
-✅ IoT automation thinking
-
-What I love about this project — it's not just 
-code, it's actual logic that smart home devices use!
-
-CS Student @ UET Lahore 🎓
-Day [X] of my Python Journey
-
-#Python #SmartHome #IoT #100DaysOfCode
-#UETLahore #Programming #ApnaCollege
-#ConditionalStatements #BeginnerProjects"""
